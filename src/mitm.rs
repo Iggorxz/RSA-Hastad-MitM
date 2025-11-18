c79a1d3999dd143ecaf0982e4879d88602578d01
Volodymyr Herashchenko <cholkegn@gmail.com>
2025-11-18 13:47:03 +0200

    add main mitm method
    also change vec to hashmap for faster search

--- src/mitm.rs
+++ src/mitm.rs
@@ -1,13 +1,29 @@
-use num_bigint::BigUint;
-use num_traits::Num;
+use std::collections::HashMap;
 
-pub fn precalculation(l: u32, e: u32, n: BigUint) -> Vec<num_bigint::BigUint> {
-    let mut result = Vec::with_capacity(1 << (l / 2));
+use num_bigint::BigUint;
+
+pub fn precalculation(l: u32, e: u32, n: &BigUint) -> HashMap<num_bigint::BigUint, u64> {
+    let mut result = HashMap::with_capacity(1 << (l / 2));
     let e_big = BigUint::from(e);
 
-    for i in 0..(1u64 << (l / 2)) {
-        result.push(BigUint::from(i).modpow(&e_big, &n));
+    for i in 1..=(1u64 << (l / 2)) {
+        result.insert(BigUint::from(i).modpow(&e_big, n), i);
     }
 
     result
+}
+
+pub fn calculation(
+    precalc: HashMap<num_bigint::BigUint, u64>,
+    c: &BigUint,
+    n: &BigUint,
+) -> Option<BigUint> {
+    for (ele, index) in &precalc {
+        let c_s = (c * ele.modinv(n).unwrap()) % n;
+
+        if let Some(j) = precalc.get(&c_s) {
+            return Some((index * j).into());
+        }
+    }
+    None
 }
