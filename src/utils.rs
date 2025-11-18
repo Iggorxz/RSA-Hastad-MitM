1bd62753a2583f203d044982ef3df2157bd1ea84
Iggorxz <ivvoloshin16@gmail.com>
2025-11-18 13:46:00 +0200

    Implement n-th root finder

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,7 @@
 use num_bigint::BigInt;
 use num_traits::{Zero, One};
 use num_integer::Integer;
+use std::cmp::Ordering;
 
 pub fn chinese_remainder_theorem(ciphertexts: Vec<BigInt>, modules: Vec<BigInt>) -> BigInt {
     let mut c = BigInt::zero();
@@ -30,4 +31,24 @@ pub fn chinese_remainder_theorem(ciphertexts: Vec<BigInt>, modules: Vec<BigInt>)
 
     c = c.mod_floor(&n);
     return c;
+}
+
+pub fn root(num: &BigInt, deg: u32) -> BigInt {
+    let bit_len = num.bits();
+    let upper_bits = ((bit_len + deg as u64 - 1) / deg as u64) + 1;
+    let mut low = BigInt::one();
+    let mut high = BigInt::one() << (upper_bits as usize);
+
+    while low < high {
+        let mid: BigInt = (&low + &high) >> 1_i32;
+        let p = mid.pow(deg);
+
+        match p.cmp(num) {
+            Ordering::Equal => return mid,
+            Ordering::Less => low = mid + 1,
+            Ordering::Greater => high = mid,
+        }
+    }
+
+    return low - 1;
 }
